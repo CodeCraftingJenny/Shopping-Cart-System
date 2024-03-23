fab21dabfa55a6de23ef1dd697d99e25dfb95798
Jenny Wong <wjenny00@students.cs.ubc.ca>
2024-03-22 19:32:14 -0700

    Added specifications, updated ReadMe with instructions for grader

--- README.md
+++ README.md
@@ -18,3 +18,18 @@ User Stories
 - As a customer, I want to have the option to save my shopping cart to file
 - As a customer, when I return to the store, I want to be given the option to load my shopping cart from file
 
+**Instructions for grader**
+
+- You can browse and select the range of clothing by clicking into the clothing buttons, for example if you wanted
+to browse hoodies, you can do that by pressing the "Hoodie" button to see what sizes and colours are available. 
+- You can choose the size and colour by selecting an option from the drop-down menu after you have clicked into an item. 
+- You can order multiple items by clicking on the cart button, then by pressing on the order button.
+- You can add items to the cart pressing into the clothing option and pressing add cart after you have chosen your 
+preferred colour and size. 
+- You can remove item by inputting the index of the item that you want removed. For example, if there are 2 items in 
+your cart, and you want to remove the second item, you can input 2 into the field and press remove. 
+- To view the items in the cart simply click on the cart button
+- To save what you have in your cart click on the save option in the cart menu
+- To load what you had previously in cart, click on the load button.
+- You can locate my visual component when you first run the program. There are pictures of clothing, and if you want to
+view different colours, you can choose a colour and it will show what the item looks like in that colour. 
--- src/main/ui/MainMenu.java
+++ src/main/ui/MainMenu.java
@@ -30,11 +30,14 @@ public class MainMenu extends JFrame {
     private final String dataFile = "./data/customer.json";
     private Customer customer;
 
+    //EFFECTS: Constructor that creates a customer and the menuGUI
     public MainMenu() {
         customer = new Customer("JJ");
         menuGUI();
     }
 
+    //REQUIRES: String that represents the image path to image, ints of width and height
+    //EFFECTS: loads image, resizes image, and returns that new image
     private ImageIcon resizeImage(String imagePath, int width, int height) {
         ImageIcon originalIcon = new ImageIcon(MainMenu.class.getResource(imagePath));
         Image originalImage = originalIcon.getImage();
@@ -42,6 +45,7 @@ public class MainMenu extends JFrame {
         return new ImageIcon(scaledImage);
     }
 
+    //EFFECTS: creates a combo box where the user can choose a size
     private JComboBox<String> createSizeComboBox() {
         String[] sizes = {"S", "M", "L"};
         sizeComboBox = new JComboBox<>(sizes);
@@ -49,6 +53,7 @@ public class MainMenu extends JFrame {
         return sizeComboBox;
     }
 
+    //EFFECTS: creates a combo box where the user can choose a colour
     private JComboBox<String> createColourComboBox() {
         String[] colours = {"white", "black"};
         colourComboBox = new JComboBox<>(colours);
@@ -56,6 +61,7 @@ public class MainMenu extends JFrame {
         return colourComboBox;
     }
 
+    //EFFECTS: creates a label of hat price
     private JLabel hatPrice() {
         JLabel priceHat = new JLabel();
         priceHat.setText("$15.00");
@@ -64,6 +70,7 @@ public class MainMenu extends JFrame {
         return priceHat;
     }
 
+    //EFFECTS: creates a label of hoodie price
     private JLabel hoodiePrice() {
         JLabel priceHoodie = new JLabel();
         priceHoodie.setText("$50.00");
@@ -72,6 +79,7 @@ public class MainMenu extends JFrame {
         return priceHoodie;
     }
 
+    //EFFECTS: creates a label of tote price
     private JLabel totePrice() {
         JLabel priceBag = new JLabel();
         priceBag.setText("$10.00");
@@ -80,7 +88,8 @@ public class MainMenu extends JFrame {
         return priceBag;
     }
 
-
+    //EFFECTS: iterates over the items in the cart, creates a string that appends each item in cart
+    // modifies the message so that it fits in the screen, returns the message
     private JLabel displayMessages() {
         StringBuilder messageBuilder = new StringBuilder("<html>");
         for (Clothing clothing : customer.viewCart()) {
@@ -99,11 +108,13 @@ public class MainMenu extends JFrame {
         return messageLabel;
     }
 
+    //EFFECTS: creates a label of total price
     private JLabel displayTotalPrice() {
         JLabel totalLabel = new JLabel("Total Price: $" + customer.getTotal());
         return totalLabel;
     }
 
+    //EFFECTS: creates a hoodie button, if pressed it will open hoodie frame
     private JButton createHoodieButton() {
         JButton hoodieButton = new JButton("Hoodie");
         hoodieButton.setBounds(175, 270, 200, 30);
@@ -116,6 +127,7 @@ public class MainMenu extends JFrame {
         return hoodieButton;
     }
 
+    //EFFECTS: creates a cap button,if pressed it will open cap frame
     private JButton createCapButton() {
         JButton capButton = new JButton("Cap");
         capButton.setBounds(575, 270, 200, 30);
@@ -128,6 +140,7 @@ public class MainMenu extends JFrame {
         return capButton;
     }
 
+    //EFFECTS: creates a tote bag button, if pressed it will open tote bag frame
     private JButton createToteBagButton() {
         JButton toteBag = new JButton("Tote Bag");
         toteBag.setBounds(175, 530, 200, 30);
@@ -140,6 +153,7 @@ public class MainMenu extends JFrame {
         return toteBag;
     }
 
+    //EFFECTS: creates a beanie button, if pressed it will open beanie frame
     private JButton createBeanieButton() {
         JButton beanie = new JButton("Beanie");
         beanie.setBounds(575, 530, 200, 30);
@@ -152,6 +166,8 @@ public class MainMenu extends JFrame {
         return beanie;
     }
 
+    //REQUIRES: two jlabels of white and black beanie
+    //EFFECTS: createss a white button for beanie, if pressed it sets the white beanie visbility to true
     private JButton createWhiteBeanieButton(JLabel whiteBeanie, JLabel blackBeanie) {
         JButton whiteButton = new JButton("White");
         whiteButton.setBounds(550, 100, 70, 30);
@@ -167,7 +183,7 @@ public class MainMenu extends JFrame {
         return whiteButton;
     }
 
-
+    //EFFECTS: creates a shopping cart button on the main page, if pressed it will open cart frame
     private JButton createShoppingCartButton() {
         JButton cart = new JButton("Cart");
         cart.setBounds(875, 30, 70, 30);
@@ -180,7 +196,21 @@ public class MainMenu extends JFrame {
         return cart;
     }
 
+    //EFFECTS: creates a exit button on the main page, if pressed it will close the program
+    private JButton createExitButton() {
+        JButton exit = new JButton("Exit");
+        exit.setBounds(775, 30, 70, 30);
+        exit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        return exit;
+    }
 
+    //REQUIRES: two jlabels of white and black hoodie
+    //EFFECTS: createss a white button for hoodie, if pressed it sets the white hoodie visbility to true
     private JButton createWhiteHoodieButton(JLabel whiteHoodie, JLabel blackHoodie) {
         JButton whiteButton = new JButton("White");
         whiteButton.setBounds(550, 100, 70, 30);
@@ -196,6 +226,8 @@ public class MainMenu extends JFrame {
         return whiteButton;
     }
 
+    //REQUIRES: two jlabels of white and black hoodie
+    //EFFECTS: createss a black button for hoodie, if pressed it sets the black hoodie visbility to true
     private JButton createBlackHoodieButton(JLabel whiteHoodie, JLabel blackHoodie) {
         JButton blackButton = new JButton("Black");
         blackButton.setBounds(650, 100, 70, 30);
@@ -211,6 +243,8 @@ public class MainMenu extends JFrame {
         return blackButton;
     }
 
+    //MODIFIES: customer, itemPrices, totalPrice
+    //EFFECTS: adds clothing item to cart, also adds total prices
     private void addToCart(String selectedItem, String colour, String size) {
         Clothing clothing = new Clothing(selectedItem, colour, size);
         String message = selectedItem + " added to cart";
@@ -220,6 +254,7 @@ public class MainMenu extends JFrame {
         totalPrice += clothing.getPrice();
     }
 
+    //EFFECTS: Creates button to add items to cart, if pressed, item will be added to cart
     private JButton createAddToCartButton(Clothing itemName, double price) {
         JButton cart = new JButton("Add Cart");
         cart.setBounds(550, 300, 85, 30);
@@ -233,6 +268,7 @@ public class MainMenu extends JFrame {
         return cart;
     }
 
+    //EFFECTS: createss a white button for cap, if pressed it sets the white cap visbility to true
     private JButton createWhiteCapButton(JLabel whiteCap, JLabel blackCap) {
         JButton whiteButton = new JButton("White");
         whiteButton.setBounds(550, 100, 70, 30);
@@ -248,6 +284,7 @@ public class MainMenu extends JFrame {
         return whiteButton;
     }
 
+    //EFFECTS: createss a black button for cap, if pressed it sets the black cap visbility to true
     private JButton createBlackCapButton(JLabel whiteCap, JLabel blackCap) {
         JButton blackButton = new JButton("Black");
         blackButton.setBounds(650, 100, 70, 30);
@@ -263,6 +300,7 @@ public class MainMenu extends JFrame {
         return blackButton;
     }
 
+    //EFFECTS: createss a white button for totebag, if pressed it sets the white totebag visbility to true
     private JButton createWhiteToteButton(JLabel whiteTote, JLabel blackTote) {
         JButton whiteButton = new JButton("White");
         whiteButton.setBounds(550, 100, 70, 30);
@@ -278,6 +316,7 @@ public class MainMenu extends JFrame {
         return whiteButton;
     }
 
+    //EFFECTS: createss a black button for totebag, if pressed it sets the black totebag visbility to true
     private JButton createBlackToteButton(JLabel whiteTote, JLabel blackTote) {
         JButton blackButton = new JButton("Black");
         blackButton.setBounds(650, 100, 70, 30);
@@ -293,7 +332,7 @@ public class MainMenu extends JFrame {
         return blackButton;
     }
 
-
+    //EFFECTS: createss a black button for beanie, if pressed it sets the black beanie visbility to true
     private JButton createBlackBeanieButton(JLabel whiteBeanie, JLabel blackBeanie) {
         JButton blackButton = new JButton("Black");
         blackButton.setBounds(650, 100, 70, 30);
@@ -309,6 +348,7 @@ public class MainMenu extends JFrame {
         return blackButton;
     }
 
+    //EFFECTS: creates a button where customers can order item, if pressed, it will open the order frame
     private JButton orderItem() {
         JButton order = new JButton("Order Items");
         order.setBounds(700, 400, 120, 30);
@@ -321,6 +361,7 @@ public class MainMenu extends JFrame {
         return order;
     }
 
+    //EFFECTS: creates a button where user can save what's in their cart
     private JButton saveButton() {
         JButton saveButton = new JButton("Save");
         saveButton.setBounds(700, 450, 100, 30);
@@ -334,6 +375,7 @@ public class MainMenu extends JFrame {
         return saveButton;
     }
 
+    //EFFECTS: creates a button where user can load what's in their cart
     private JButton loadButton() {
         JButton loadButton = new JButton("Load");
         loadButton.setBounds(820, 450, 100, 30);
@@ -347,8 +389,10 @@ public class MainMenu extends JFrame {
         return loadButton;
     }
 
-    private void openCapFrame() {
 
+    //MODIFIES: JFrame
+    //EFFECTS: creates a frame of the cap item with all of its features
+    private void openCapFrame() {
         JFrame capFrame = setUpFrame("Cap Frame");
         ImageIcon whiteCap = resizeImage("whiteCap.png", 250, 200);
         JLabel capWhite = getCapWhite(whiteCap);
@@ -369,6 +413,7 @@ public class MainMenu extends JFrame {
         capFrame.add(hatPrice());
     }
 
+    //EFFECTS: creates a label of a white cap
     private JLabel getCapWhite(ImageIcon whiteCap) {
         JLabel capWhite = new JLabel();
         capWhite.setIcon(whiteCap);
@@ -377,6 +422,7 @@ public class MainMenu extends JFrame {
         return capWhite;
     }
 
+    //EFFECTS: creates a label of a black cap
     private JLabel getCapBlack() {
         ImageIcon blackCap = resizeImage("blackCap.png", 250, 200);
         JLabel capBlack = new JLabel();
@@ -386,6 +432,7 @@ public class MainMenu extends JFrame {
         return capBlack;
     }
 
+    //EFFECTS: creates a label of a default white cap
     private JLabel getDefCap(ImageIcon whiteCap) {
         ImageIcon defaultCap = resizeImage("whiteCap.png", 250, 200);
         JLabel defCap = new JLabel();
@@ -395,6 +442,8 @@ public class MainMenu extends JFrame {
         return defCap;
     }
 
+    //MODIFIES: JFrame
+    //EFFECTS: creates a frame of the hoodie item with all of its features
     private void openHoodieFrame() {
         JFrame hoodieFrame = setUpFrame("Hoodie Frame");
         ImageIcon newHoodie = resizeImage("hoodie.png", 500, 500);
@@ -419,6 +468,7 @@ public class MainMenu extends JFrame {
         hoodieFrame.add(hoodiePrice());
     }
 
+    //EFFECTS: creates a label of a default white hoodie
     private JLabel getDefHoodie(ImageIcon newHoodie) {
         ImageIcon defaultHoodie = resizeImage("hoodie.png", 500, 500);
         JLabel defHoodie = new JLabel();
@@ -428,6 +478,7 @@ public class MainMenu extends JFrame {
         return defHoodie;
     }
 
+    //EFFECTS: creates a label of a black cap
     private JLabel getBlackHoodie() {
         ImageIcon newBlackHoodie = resizeImage("blackhoodie.png", 500, 500);
         JLabel blackHoodie = new JLabel();
@@ -437,6 +488,8 @@ public class MainMenu extends JFrame {
         return blackHoodie;
     }
 
+    //MODIFIES: JFrame
+    //EFFECTS: creates a frame according to specifications
     private JFrame setUpFrame(String createFrame) {
         JFrame newFrame = new JFrame(createFrame);
         newFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -447,6 +500,8 @@ public class MainMenu extends JFrame {
         return newFrame;
     }
 
+    //MODIFIES: JFrame
+    //EFFECTS: creates a frame of the tote bag item with all of its features
     private void openToteBagFrame() {
         JFrame toteBagFrame = setUpFrame("Tote Bag Frame");
         ImageIcon whiteToteBag = resizeImage("wTote.png", 400, 300);
@@ -471,6 +526,7 @@ public class MainMenu extends JFrame {
         toteBagFrame.add(totePrice());
     }
 
+    //EFFECTS: creates a label of a black tote bag
     private JLabel getBlackTote() {
         ImageIcon blackToteBag = resizeImage("bTote.png", 400, 300);
         JLabel blackTote = new JLabel();
@@ -480,6 +536,7 @@ public class MainMenu extends JFrame {
         return blackTote;
     }
 
+    //EFFECTS: creates a label of a white tote bag
     private JLabel getDefTote(ImageIcon whiteToteBag) {
         ImageIcon defaultTote = resizeImage("wTote.png", 400, 300);
         JLabel defTote = new JLabel();
@@ -489,6 +546,8 @@ public class MainMenu extends JFrame {
         return defTote;
     }
 
+    //MODIFIES: JFrame
+    //EFFECTS: creates a frame of the beanie item with all of its features
     private void openBeanieFrame() {
         JFrame beanieFrame = setUpFrame("Beanie Frame");
 
@@ -518,6 +577,7 @@ public class MainMenu extends JFrame {
         beanieFrame.add(hatPrice());
     }
 
+    //EFFECTS: creates a label of a black beanie
     private JLabel getBeanieBlack() {
         ImageIcon blackBeanie = resizeImage("bBeanie.png", 400, 300);
         JLabel beanieBlack = new JLabel();
@@ -527,6 +587,7 @@ public class MainMenu extends JFrame {
         return beanieBlack;
     }
 
+    //EFFECTS: creates a label of a white beanie
     private JLabel getDefBeanie(ImageIcon whiteBeanie, JLabel beanieWhite) {
         ImageIcon defaultTote = resizeImage("wBeanie.png", 400, 300);
         JLabel defBeanie = new JLabel();
@@ -536,7 +597,9 @@ public class MainMenu extends JFrame {
         return defBeanie;
     }
 
-
+    //MODIFIES: JFrame
+    //EFFECTS: creates a frame of the cart frame, where user can view what's in their cart
+    // remove items, save, and load their cart
     private void openCartFrame() {
         JFrame cartFrame = setUpFrame("Cart Frame");
         JLabel indexLabel = new JLabel("Enter index of item you want removed: ");
@@ -561,6 +624,8 @@ public class MainMenu extends JFrame {
         cartFrame.add(loadButton());
     }
 
+    //MODIFIES: customer
+    //EFFECTS: removes item in cart according to the given index
     private void removeFromCart(int index) {
         if (index >= 0 && index < customer.viewCart().size()) {
             Clothing removeItem = customer.viewCart().remove(index);
@@ -568,9 +633,12 @@ public class MainMenu extends JFrame {
             totalPrice -= removePrice;
             JOptionPane.showMessageDialog(null,
                     removeItem.getNameOfItem() + " has been removed. Please open cart again.");
+        } else {
+            JOptionPane.showMessageDialog(null, "Invalid");
         }
     }
 
+    //EFFECTS: Creates a remove button, if pressed, item will be removed
     private JButton createRemoveButton() {
         JButton removeButton = new JButton("Remove");
         removeButton.addActionListener(new ActionListener() {
@@ -578,8 +646,8 @@ public class MainMenu extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 String indexString = indexField.getText();
                 if (!indexString.isEmpty()) {
-                    int indexInt = Integer.parseInt(indexString) - 1;
-                    removeFromCart(indexInt);
+                    int indexInt = Integer.parseInt(indexString);
+                    removeFromCart(indexInt - 1);
                 } else {
                     JOptionPane.showMessageDialog(null, "Invalid");
                 }
@@ -589,6 +657,8 @@ public class MainMenu extends JFrame {
         return removeButton;
     }
 
+    //MODIFIES: JFrame
+    //EFFECTS: creates a frame of the menu with all of its features
     public void menuGUI() {
         JLabel hoodie = getHoodie();
         JLabel cap = getCap();
@@ -611,8 +681,10 @@ public class MainMenu extends JFrame {
         frame.add(welcome);
 
         frame.add(createShoppingCartButton());
+        frame.add(createExitButton());
     }
 
+    //EFFECTS: returns the hoodie image as a label
     private JLabel getHoodie() {
         ImageIcon newHoodie = resizeImage("hoodie.png", 150, 150);
         JLabel hoodie = new JLabel();
@@ -621,6 +693,7 @@ public class MainMenu extends JFrame {
         return hoodie;
     }
 
+    //EFFECTS: returns the cap image as a label
     private JLabel getCap() {
         ImageIcon newCap = resizeImage("cap.png", 150, 100);
         JLabel cap = new JLabel();
@@ -629,6 +702,7 @@ public class MainMenu extends JFrame {
         return cap;
     }
 
+    //EFFECTS: returns the tote bag image as a label
     private JLabel getToteBag() {
         ImageIcon newToteBag = resizeImage("totebag.png", 150, 150);
         JLabel toteBag = new JLabel();
@@ -637,6 +711,7 @@ public class MainMenu extends JFrame {
         return toteBag;
     }
 
+    //EFFECTS: returns the beanie image as a label
     private JLabel getBeanie() {
         ImageIcon newBeanie = resizeImage("beanie.png", 150, 150);
         JLabel beanie = new JLabel();
@@ -645,6 +720,8 @@ public class MainMenu extends JFrame {
         return beanie;
     }
 
+    //MODIFIES: JFrame
+    //EFFECTS: creates a JFrame
     private JFrame setFrame() {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -654,6 +731,7 @@ public class MainMenu extends JFrame {
         return frame;
     }
 
+    //EFFECTS: creates a welcome label in main menu
     private JLabel getWelcomeLabel(JFrame frame) {
         JLabel welcome = new JLabel("Welcome");
         welcome.setFont(new Font("Arial", Font.BOLD, 30));
@@ -663,6 +741,8 @@ public class MainMenu extends JFrame {
     }
 
 
+    //MODIFIES: JFrame
+    //EFFECTS: creates a frame of the order menu, where users can input their information
     private void orderFrame() {
         JFrame orderFrame = setUpFrame("Order Frame");
         nameField = new JTextField(2);
@@ -720,6 +800,8 @@ public class MainMenu extends JFrame {
         return confirmButton;
     }
 
+    //MODIFIES: this
+    //EFFECTS: saves cart into file
     private void saveCart() {
         JsonWriter jsonWriter = new JsonWriter(dataFile);
         try {
@@ -732,7 +814,7 @@ public class MainMenu extends JFrame {
     }
 
     // MODIFIES: this
-    // EFFECTS: loads workroom from file
+    // EFFECTS: loads cart from file
     private void loadCart() {
         JsonReader jsonReader = new JsonReader(dataFile);
         try {
